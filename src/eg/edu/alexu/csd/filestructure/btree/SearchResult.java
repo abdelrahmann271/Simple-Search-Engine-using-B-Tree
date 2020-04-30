77fa44192d929306436f91c9d2d9066a5a3b6cba
Abdelrahman Nour <abdelrahmann271@gmail.com>
2020-04-30 17:36:20 +0200

    Final Commit

--- src/eg/edu/alexu/csd/filestructure/btree/SearchResult.java
+++ src/eg/edu/alexu/csd/filestructure/btree/SearchResult.java
@@ -3,29 +3,36 @@ package eg.edu.alexu.csd.filestructure.btree;
 import javax.management.RuntimeErrorException;
 
 public class SearchResult implements ISearchResult {
+    private String id ;
+    private int rank ;
+    public SearchResult(String id,int rank) {
+        if (id == null||rank<0) throw new RuntimeErrorException(new Error());
+        this.id = id ;
+        this.rank = rank;
+    }
 
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public SearchResult() {
+    }
 
-	@Override
-	public void setId(String id) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public String getId() {
+        return this.id;
+    }
 
-	@Override
-	public int getRank() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public void setId(String id) {
+        if (id == null) throw new RuntimeErrorException(new Error());
+        this.id = id ;
+    }
 
-	@Override
-	public void setRank(int rank) {
-		// TODO Auto-generated method stub
-		
-	}
-   
+    @Override
+    public int getRank() {
+        return rank;
+    }
+
+    @Override
+    public void setRank(int rank) {
+        if (rank<0) throw new RuntimeErrorException(new Error());
+        this.rank = rank;
+    }
 }
